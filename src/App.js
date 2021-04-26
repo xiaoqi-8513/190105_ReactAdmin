11a85d1ef6758d4449f48b894900b0a2373e05c9
吴逆天 <1016125009@qq.com>
2021-04-26 17:09:34 +0800

    init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import { Button,message } from 'antd'   // 使用的组件
+// 应用的根组件
+ 
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleClick = () => {
+      message.success('成功啦.....')
+  }
+  render(){
+    return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+  }
 }
-
-export default App;
